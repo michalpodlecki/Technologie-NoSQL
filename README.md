fad53c6c5acac0303dff036ca6955a713d293b38
Michal Podlecki <m.podlecki@op.pl>
2012-04-29 18:12:51 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Technologie NoSQL
 =================
+Repozytorium na potrzeby laboratorium - semestr letni 2012.
+----
+* Skrypt ruby zapisujący dane do bazy MongoDB:
+`ruby import2mongo.db [port]`
 
+* Skrypt ruby przenoszący zapisane dane do bazy CouchDB:
+`ruby mongo2couch.rb [port serwera mongodb] [port serwera couchdb]`
--- import2mongo.rb
+++ import2mongo.rb
@@ -1,7 +1,16 @@
 require "mongo"
 require "json"
 
-connection = Mongo::Connection.new("localhost",40012)
+if ARGV[0] == "-h" || ARGV[0] == "--help" || !ARGV[0] || ARGV[0].to_i == 0 then
+  puts "\nWywolanie skryptu:\n"
+  puts "> ruby #{__FILE__} [port]"
+  puts
+  exit
+end
+
+port = ARGV[0]
+
+connection = Mongo::Connection.new("localhost",port)
 
 db = connection.db("nosql-lab-db")
 
@@ -11,6 +20,8 @@ coll = db.collection("movies")
 coll.remove
 
 file = File.new "filmy_movies.json"
+
+puts "Import danych w toku..."
 
 file.each_line do |line|
   line = line[1...-1]
@@ -23,10 +34,12 @@ file.each_line do |line|
   index = 0
   arrayOfJSONs.each do |value|
     json = JSON.parse(value)
-    puts json
+    #puts json
     coll.insert(json)
     index += 1
     break if index == 5000
   end
 end
 
+puts "---"
+puts "Import danych zakonczony."
