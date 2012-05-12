abbb82962535c753e1c6cbf3cee192e92771e076
Michal Podlecki <m.podlecki@op.pl>
2012-05-12 19:50:44 +0200

    poprawki

--- import2mongo.rb
+++ import2mongo.rb
@@ -1,25 +1,53 @@
 require "mongo"
 require "json"
 
-if ARGV[0] == "-h" || ARGV[0] == "--help" || !ARGV[0] || ARGV[0].to_i == 0 then
+if ARGV[0] == "-h" || ARGV[0] == "--help" || ARGV.size == 0 then
   puts "\nWywolanie skryptu:\n"
-  puts "> ruby #{__FILE__} [port]"
+  puts "> ruby #{__FILE__} json_file [-p port][ -d database][ -c collection]"
   puts
   exit
 end
 
-port = ARGV[0]
+filename = ARGV[0].downcase
 
-connection = Mongo::Connection.new("localhost",port)
+if File.extname(filename) != ".json"
+  puts "Nieprawidlowe rozszerzenie pliku JSON"
+  exit
+end
 
-db = connection.db("nosql-lab-db")
+port, database, collection = 27017, "test", "movies"
+options = {"-p" => port,"-d" => database,"-c" => collection}
 
-coll = db.collection("movies")
+if ARGV.size > 0
+  size = ARGV.size
+  arg = ARGV[0]
+  if size % 2 == 1
+    options.each do |key,val|
+      index = ARGV.index(key)
+      if index then
+        options[key] = ARGV[index+1]
+      end
+    end
+  else
+    puts "Niepoprawna ilosc argumentow"
+    exit
+  end
+end
+
+puts "[Settings]"
+puts "port:\t\t#{options["-p"]}\ndatabase:\t#{options["-d"]}\ncollection:\t#{options["-c"]}"
+puts
+
+connection = Mongo::Connection.new("localhost",options["-p"])
+
+db = connection.db(options["-d"])
+
+coll = db.collection(options["-c"])
 
 # drop all documents first if exists
 coll.remove
 
-file = File.new "filmy_movies.json"
+file = File.new filename
 
 puts "Import danych w toku..."
 
@@ -34,12 +62,10 @@ file.each_line do |line|
   index = 0
   arrayOfJSONs.each do |value|
     json = JSON.parse(value)
-    #puts json
     coll.insert(json)
     index += 1
     break if index == 5000
   end
 end
 
-puts "---"
 puts "Import danych zakonczony."
